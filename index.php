45010ab237745899098d3238adddea3695c25082
Masudul Hasan Masud <masudulhasanmasud690@gmail.com>
2021-09-22 22:24:15 +0200

    add RSA code

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+	// ini_set('memory_limit', '3G'); 
+	function rsa_computation($limit){
+		$p = 178542003245811211274167228297361192303886321036074276889145691522634525820185614278499562592134188995169731066418203258297035264969457638591284906658912408319763156912951486020761069099132619194489006875108217247513715271974383296142805846405783845170862140174184507256128825312324419293575432423822703857091;
+		$r = 187;
+		for ($i = 1; $i < $limit; $i++) {
+		$p=($p*$p)%$r;
+		}
+		return $p;
+	}
 
-use Cowsayphp\Farm;
-
-header('Content-Type: text/plain');
-
-$text = "Set a message by adding ?message=<message here> to the URL";
-if(isset($_GET['message']) && $_GET['message'] != '') {
-	$text = htmlspecialchars($_GET['message']);
-}
-
-$cow = Farm::create(\Cowsayphp\Farm\Cow::class);
-echo $cow->say($text);
+	$start = microtime(true);
+	if(isset($_GET['id'])){
+		$val = urldecode($_GET["id"]);
+		if (!empty($val)){
+		rsa_computation($val);
+		}
+	}
+	$time_elapsed_secs = microtime(true) - $start;
+	echo "total time  taken = ".($time_elapsed_secs*1000) . " ms\n";
+?>
